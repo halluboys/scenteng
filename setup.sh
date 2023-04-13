d6e92ba31625bedcda0dcac1e35b40b2ecd7b143
halluboys <87511503+halluboys@users.noreply.github.com>
2023-04-14 00:10:40 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -56,7 +56,7 @@ wget https://${akbarvpn}/cf.sh && chmod +x cf.sh && ./cf.sh
 #install v2ray
 wget https://${akbarvpnnnnnn}/ins-xray1.sh && chmod +x ins-xray1.sh && screen -S xray ./ins-xray1.sh
 #install ssh ovpn
-wget https://${akbarvpn}/ins.sh && chmod +x ins.sh && screen -S ssh-vpn ./ins.sh
+wget https://${akbarvpn}/ssh-vpn1.sh && chmod +x ssh-vpn1.sh && screen -S ssh-vpn ./ssh-vpn1.sh
 #wget https://${akbarvpnn}/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
 #wget https://${akbarvpnnn}/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
